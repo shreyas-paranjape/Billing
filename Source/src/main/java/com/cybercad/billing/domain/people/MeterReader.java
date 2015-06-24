d7ef4e9de8fbbca914338386edf149448a99050e
Shreyas Paranjape <sparanjape89@gmail.com>
2015-06-24 12:10:38 +0530

    removed person class

--- Source/src/main/java/com/cybercad/billing/domain/people/MeterReader.java
+++ Source/src/main/java/com/cybercad/billing/domain/people/MeterReader.java
@@ -5,7 +5,9 @@ import java.util.Set;
 import com.cybercad.billing.domain.comms.CommunicationDetails;
 import com.cybercad.billing.domain.geoclass.Ward;
 
-public class MeterReader extends Person {
+public class MeterReader{
+	private PersonalDetails personalDetails;
+	private CommunicationDetails communicationDetails;
 	private Set<Ward> wards;
 	
 	private void download(){
--- Source/src/main/java/com/cybercad/billing/domain/people/Person.java
+++
@@ -1,9 +0,0 @@
-package com.cybercad.billing.domain.people;
-
-import com.cybercad.billing.domain.comms.CommunicationDetails;
-
-public class Person {
-
-	protected PersonalDetails personalDetails;
-	protected CommunicationDetails communicationDetails;
-}
